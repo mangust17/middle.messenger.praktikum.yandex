c829a0ca1b429002fadd6db8cdc188296b10c5b9
mangust <matvejlejko5@gmail.com>
2025-04-03 12:19:39 +0300

    9

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,17 +1,39 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  root: '.',
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
+  plugins: [
+    handlebars({
+      partialDirectory: [
+        resolve(__dirname, 'src/components'),
+        resolve(__dirname, 'src/pages'),
+        resolve(__dirname, 'src'), 
+      ],
+    }),
+  ],
   assetsInclude: ['**/*.hbs'],
   build: {
     rollupOptions: {
-      input: 'src/index.ts',
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    assetsDir: 'assets',
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
+  publicDir: 'static',
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, 'src/components'),
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@utils': resolve(__dirname, 'src/utils'),
+      '@types': resolve(__dirname, 'src/types'),
+      '@core': resolve(__dirname, 'src/core'),
+      '@models': resolve(__dirname, 'src/models'),
     },
   },
 });
